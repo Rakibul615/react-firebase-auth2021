9a4d86b8a6663426da2a45b505ea660c5a6f5cc6
Rakibul615 <rakibul01072019@gmail.com>
2021-10-16 16:18:49 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
+import {getAuth, signInWithPopup, GoogleAuthProvider,GithubAuthProvider,signOut  } from "firebase/auth";
+import { useState } from "react";
 import './App.css';
+import initializeAuthentication from './Firebase/firebase.init';
+
+
+const googleProvider = new GoogleAuthProvider();
+const gitHubProvider = new GithubAuthProvider();
+initializeAuthentication();
+
 
 function App() {
+  const [user,setUser]=useState({});
+  const auth = getAuth();   //this function must have inside the app() other its given error
+
+  // this part is gitHub 
+
+  const handleGitHubSignIn=()=>{
+    signInWithPopup(auth, gitHubProvider)
+    .then((result)=>{
+      const {displayName,photoURL,email,uid}=result.user;
+      const loggedInUser={
+        name:displayName,
+        img:photoURL,
+        email: email,
+        userId:uid
+      }
+      setUser(loggedInUser);
+     // console.log(user);  // we do not get name and email from gitHub that's why name and email is not show on display .when we make only img tag its showing on tah display but concept is right.
+     
+    })
+    .catch(error=>{
+      const errorCode=error.code;
+      console.log(errorCode);
+    })
+
+  }
+
+
+  // this part is google 
+  const handleGoogleSignIn=()=>{
+    
+    signInWithPopup(auth, googleProvider)
+    .then((result)=>{
+      const {displayName,email,photoURL,uid}=result.user;
+      const loggedInUser={
+        name:displayName,
+        email:email,
+        img: photoURL,
+        userId:uid
+      }
+      setUser(loggedInUser);
+      console.log(result.user);
+
+    })
+    .catch(error=>{
+      const errorCode=error.code;
+      console.log(errorCode);
+    })
+   
+
+
+  }
+  const handleSignOut=()=>{
+    signOut(auth)
+    .then(() => {
+      setUser({});
+    
+    })
+    .catch((error) => {
+      console.log(error.code);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {
+      ! user.userId ?  <div>
+       <button onClick={handleGoogleSignIn}> Google sign in</button>
+       <button onClick={handleGitHubSignIn}> GitHub sign in</button>
+       </div> : <button onClick={handleSignOut}>sign out</button>
+     }
+      
+      
+      <br />    
+      {
+        user.email && <div>    
+          welcome {user.name}
+          <h3>Your email: {user.email}</h3>
+          <img src={user.img} alt="" />
+        </div>
+      }
+      
     </div>
+    
   );
 }
 
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB0bp2K7z3oFgpK_bwXs5c8T_R7Mr95T8Y",
+    authDomain: "react-firebase-authentic-3d24e.firebaseapp.com",
+    projectId: "react-firebase-authentic-3d24e",
+    storageBucket: "react-firebase-authentic-3d24e.appspot.com",
+    messagingSenderId: "79534163263",
+    appId: "1:79534163263:web:f5533069573017f48cff8a"
+  };
+  export default firebaseConfig;
